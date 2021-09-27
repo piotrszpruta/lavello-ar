57af9e74c46ed160f197255afa240f71c8c31a65
Piotr Szpruta <38475451+piotrszpruta@users.noreply.github.com>
2021-09-27 23:29:57 +0200

    Add files via upload

---
+++ wannaA/src/script.js
@@ -0,0 +1,19 @@
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+  } else {
+    progressBar.classList.remove('hide');
+    if (event.detail.totalProgress === 0) {
+      event.target.querySelector('.center-pre-prompt').classList.add('hide');
+    }
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+const ModelViewerElement = document.querySelector('model-viewer')
+      ModelViewerElement.minimumRenderScale = 1
+      ModelViewerElement.scale = '2.0 2.0 2.0'
